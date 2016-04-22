29e42a1622ff2d58fd5188e702e0bae689ca0504
Fedotov Pavel <pashka0989@yandex.ru>
2016-04-22 18:32:29 +0600

    Beta Version#2

--- Test_rand.cpp
+++ Test_rand.cpp
@@ -4,8 +4,24 @@
 #include <stdlib.h>
 #include <conio.h>
 #include <ctime>
+#include <fstream>
+#include <iostream>
 
-const int N=9;
+using namespace std; 
+
+const int N=10;
+
+void indocp(int a){
+     ofstream out("Счёт.txt",ios::app);
+     out << "\n" << a;
+     out.close();
+     }
+     
+void indocm(int* a){
+     ofstream out("Счёт.txt",ios::app);
+     out << a;
+     out.close();
+     }
 
 //Переменные для записи счёта;  
 int pscore=0,bscore=0,tscore=0; 
@@ -44,7 +60,6 @@ outstreamxy(615,230);
      
 //поле игры;
 void grid(){
-   initwindow(705,405);
    setfillstyle(1,15);
    floodfill(0,0,1);
    setcolor(0);
@@ -85,117 +100,138 @@ void zero(int a){
          if(a==9){circle(335,335,64); return;}
 }
 
-//Вычисление хода игроков;
-int play(int player, char* txt, int* m,int f,int &w){
-         if(w){setfillstyle(1,15); bar(406,275,705,330);
-         setcolor(14); outtextxy(440,280,txt);}
-         if(w&&player==3)outtextxy(470,305,"player one...");
-         if(w&&player==4)outtextxy(470,305,"player two...");
-         w=0;
-         if(m[((mousex()/136)+1)+(3*((mousey()/136)))]==0&&ismouseclick(WM_LBUTTONDOWN)){ 
-         if(player==2||player==4){w=1; setcolor(9); m[((mousex()/136)+1)+(3*((mousey()/136)))]=4; 
-         zero(((mousex()/136)+1)+(3*((mousey()/136)))); f=0;}
-         else{w=1; setcolor(12); m[((mousex()/136)+1)+(3*((mousey()/136)))]=1; 
-         cross(((mousex()/136)+1)+(3*((mousey()/136)))); f=1;}
-         }
-         return f;
-}
-
 //Другие варианты хода;
 void casual_play(int* a,int flag){
 int i;    
  if(flag==0){    
-  for(i=1;i<=N;i++){                
+  for(i=1;i<N;i++){                
    if(a[i]==0){a[i]=4; zero(i); break;}                  
   }
  } 
  else
-  for(i=1;i<=N;i++){
+  for(i=1;i<N;i++){
    if(a[i]==0){a[i]=1; cross(i); break;}                      
  }
 }
-int win(int* a, int sum,char* txt,int &scr,int kx,int ky){
-    int prob=0,s,y=-68,j,i;
-    setcolor(2);
-        //проверка выигрыша по горизонтали;
+
+//Бот против Бота! Игра Века!
+void bvb(){
+    int a[N],i,j,y=-68,prob=0,flag=0,sum,num=0,random,ncheck=0;
+    for(i=1;i<N;i++) a[i]=0;
+    score();
+    setlinestyle(SOLID_LINE,2,THICK_WIDTH); 
+     while(prob==0){           
+         //проверка выигрыша крестика по горизонтали;
+         setcolor(2);
          if(prob==0){
           for(j=0;j<7;j+=3){     
-           if((a[j+1]+a[j+2]+a[j+3])==sum){
+           if((a[j+1]+a[j+2]+a[j+3])==3){
             if(j==3) y+=135; if(j==6) y+=270;                                 
             setlinestyle(1,1,5);         
-            for(i=7;i<378;i+=7){delay(10); line(i,y+136,i+25,y+136);}
-            scr++; bgiout <<scr; outstreamxy(kx,ky); setfillstyle(1,15);
-            bar(406,280,705,400); moveto(465,280); outtext(txt); prob=1;
+            //for(i=7;i<378;i+=7){delay(10); line(i,y+136,i+25,y+136);}
+            bscore++; bgiout <<bscore; outstreamxy(605,180); setfillstyle(1,15);
+            bar(406,275,705,330); moveto(495,280); outtext("Bot One WIN!"); prob=1;
            }
           }
          }
-         if(prob==1) return 1;
-          
-         //проверка выигрыша по вертикали;
+         if(prob==1) break;
+         
+         //проверка выигрыша крестика по вертикали;
          if(prob==0){
           for(j=0;j<3;j++){
-           if((a[j+1]+a[j+4]+a[j+7])==sum){
+           if((a[j+1]+a[j+4]+a[j+7])==3){
             if(j==1) y+=135; if(j==2) y+=270;
             setlinestyle(1,1,5);         
-            for(i=7;i<378;i+=7){delay(10); line(y+136,i,y+136,i+25);}
-            scr++; bgiout <<scr; outstreamxy(kx,ky); setfillstyle(1,15);
-            bar(406,280,705,400); moveto(465,280); outtext(txt); prob=1;                           
+            //for(i=7;i<378;i+=7){delay(10); line(y+136,i,y+136,i+25);}
+            bscore++; bgiout <<bscore; outstreamxy(605,180); setfillstyle(1,15);
+            bar(406,275,705,330); moveto(495,280); outtext("Bot One WIN!"); prob=1;                           
            }
           }
          }
-         if(prob==1) return 1;
-                  
-         //проверка выигрыша по первой диагонали;
-         if(prob==0&&(a[1]+a[5]+a[9]==sum)){ 
-          setlinestyle(1,1,5);
-          for(i=7;i<378;i+=7){delay(10); line(i,i,i+25,i+25);}
-          scr++; bgiout <<scr; outstreamxy(kx,ky); setfillstyle(1,15); 
-          bar(406,280,705,400); moveto(465,280); outtext(txt); prob=1;                                           
-         }
-         if(prob==1) return 1;
+         if(prob==1) break;
          
-         //проверка выигрыша по второй диагонали;
-         if(prob==0&&(a[3]+a[5]+a[7]==sum)){ 
+         //проверка выигрыша крестика по первой диагонали;
+         if(prob==0&&(a[1]+a[5]+a[9]==3)){ 
           setlinestyle(1,1,5);
-          for(i=398,j=7;i>30;i-=7,j+=7){delay(10); line(i,j,i-25,j+25);}
-          scr++; bgiout <<scr; outstreamxy(kx,ky); setfillstyle(1,15);
-          bar(406,280,705,400); moveto(465,280); outtext(txt); prob=1;
+          //for(i=7;i<378;i+=7){delay(10); line(i,i,i+25,i+25);}
+          bscore++; bgiout <<bscore; outstreamxy(605,180); setfillstyle(1,15);
+          bar(406,275,705,330); moveto(495,280); outtext("Bot One WIN!"); prob=1;                                           
+         }
+         if(prob==1) break;
+         
+         //проверка выигрыша крестика по второй диагонали;
+         if(prob==0&&(a[3]+a[5]+a[7]==3)){ 
+          setlinestyle(1,1,5);
+          //for(i=398,j=7;i>30;i-=7,j+=7){delay(10); line(i,j,i-25,j+25);}
+          bscore++; bgiout <<bscore; outstreamxy(605,180); setfillstyle(1,15);
+          bar(406,275,705,330); moveto(495,280); outtext("Bot One WIN!"); prob=1;                                           
+         }
+         if(prob==1) break;
+         
+         //проверка выигрыша нолика по горизонтали;
+         if(prob==0){
+          for(j=0;j<7;j+=3){     
+           if((a[j+1]+a[j+2]+a[j+3])==12){
+            if(j==3) y+=135; if(j==6) y+=270;                                 
+            setlinestyle(1,1,5);         
+            //for(i=7;i<378;i+=7){delay(10); line(i,y+136,i+25,y+136);}
+            bscore+=1; bgiout <<bscore; outstreamxy(605,180); setfillstyle(1,15);
+            bar(406,275,705,330); moveto(495,280); outtext("Bot Two WIN!"); prob=1;
+           }
           }
-         if(prob==1) return 1;
+         }
+         if(prob==1) break;
+         
+         //проверка выигрыша нолика по вертикали;
+         if(prob==0){
+          for(j=0;j<3;j++){
+           if((a[j+1]+a[j+4]+a[j+7])==12){                           
+            if(j==1) y+=135; if(j==2) y+=270;
+            setlinestyle(1,1,5);         
+            //for(i=7;i<378;i+=7){delay(10); line(y+136,i,y+136,i+25);}
+            bscore+=1; bgiout <<bscore; outstreamxy(605,180);setfillstyle(1,15);
+            bar(406,275,705,330); moveto(495,280); outtext("Bot Two WIN!"); prob=1;                           
+           }
+          }
+         }
+         if(prob==1) break;
+         
+         //проверка выигрыша нолика по первой диагонали; 
+         if(prob==0&&(a[1]+a[5]+a[9]==12)){ 
+          setlinestyle(1,1,5);
+          //for(i=7;i<378;i+=7){delay(10); line(i,i,i+25,i+25);}
+          bscore+=1; bgiout <<bscore; outstreamxy(605,180); setfillstyle(1,15);
+          bar(406,275,705,330); moveto(495,280); outtext("Bot Two WIN!"); prob=1;                                           
+         }
+         if(prob==1) break;
+         
+         //проверка выигрыша нолика по второй диагонали;
+         if(prob==0&&(a[3]+a[5]+a[7]==12)){ 
+          setlinestyle(1,1,5);
+          //for(i=398,j=7;i>30;i-=7,j+=7){delay(10); line(i,j,i-25,j+25);}
+          bscore+=1; bgiout <<bscore; outstreamxy(605,180); setfillstyle(1,15);
+          bar(406,275,705,330); moveto(495,280); outtext("Bot Two WIN!"); prob=1;                                           
+         }
+         if(prob==1) break;
          
          //проверка ничьи;
          setcolor(12);
-         if(prob==0){
-          s=0;            
-          for(i=1;i<10;i++) s+=a[i];
-          if(s==21){
-           tscore++; bgiout <<tscore; outstreamxy(615,230); setfillstyle(1,15);
-           bar(406,280,705,400); moveto(465,280); outtext("Nobody has won"); prob=1;
+         if(prob==0){ 
+          sum=0;            
+          for(i=1;i<N;i++) sum+=a[i];
+          if(sum==21){  
+           tscore+=1; bgiout <<tscore; outstreamxy(615,230); setfillstyle(1,15); 
+           bar(406,275,705,330); moveto(465,280); outtext("Nobody has won");  prob=1;
           }           
          }
-         if(prob==1)return 1;
-         return 0;
-}
-
-void bvp(){
-     int a[N],i,j,y=-68,prob=0,flag=0,sum,num=1,random,ncheck=0,wrt=1;
-     for(i=1;i<=N;i++) a[i]=0;
-    score();
-    setlinestyle(SOLID_LINE,2,THICK_WIDTH);
-     while(prob==0){
-          //проверка выигрыша крестика;
-          prob=win(a,3,"Bot WIN!",bscore,605,180);
-          if(prob==1) break;
-          //проверка выигрыша нолика;
-          prob=win(a,12,"Player WIN!",pscore,600,130);
-          if(prob==1) break;
+         if(prob==1) break;
          
-     //переход на ход игрока;
-     if(flag==1) goto Player;
+         //переход на ход второго бота;
+         if(flag==1) goto bot;
          
-     //Ход Бота;
-     ncheck=0; flag=1; setcolor(14); outtextxy(420,280,"it's the bot's turn...");
-     setcolor(12); delay(800);
+        //Ход Бота;
+     ncheck=0; flag=1; setcolor(14); outtextxy(420,280,"it's the bot's turn1...");
+     setcolor(12); num++; //delay(800);
         
      //первый ход бота;
      if(num==1){
@@ -212,7 +248,7 @@ void bvp(){
      }
         
      //второй ход бота;
-     if(num==2){
+     if(num==3){
         //тактика 1;
         if((a[2]+a[6]+a[4]+a[8])==4){
          if(a[5]==0){a[5]=1; cross(5); continue;}                           
@@ -267,7 +303,7 @@ void bvp(){
      }
      
      //третий, четвёртый и пятый ход бота;
-     if(num==3||num==4||num==5){                        
+     if(num==5||num==7||num==9){                        
         //проверка парных крестиков в строке;                   
         for(j=0;j<7;j+=3){     
          if((a[j+1]+a[j+2]+a[j+3])==2){
@@ -361,46 +397,14 @@ void bvp(){
        casual_play(a,1);   
      }
      continue;
-        
-        //ход игрока;
-        Player:     
-         flag=play(2,"it's a your turn...",a,flag,wrt);
-         if(!flag) num++;
-         if(ismouseclick(WM_LBUTTONDOWN)){clearmouseclick(WM_LBUTTONDOWN); delay(20);continue;}     
-       }  
-     }
-     
-//игрок против бота;
-void pvb(){
-    int a[N],i,j,y=-68,prob=0,flag=0,sum,num=0,random,ncheck=0,wrt=1;
-    for(i=1;i<=N;i++) a[i]=0;
-    score();
-    setlinestyle(SOLID_LINE,2,THICK_WIDTH); 
-     while(prob==0){           
-         //проверка выигрыша крестика;
-          prob=win(a,3,"Player WIN!",pscore,600,130);
-          if(prob==1) break;
-          //проверка выигрыша нолика;
-          prob=win(a,12,"Bot WIN!",bscore,605,180);
-          if(prob==1) break;
-         
-         //переход на ход бота;
-         if(flag==1) goto bot;
-         
-         //ход игрока;
-         
-         flag=play(1,"it's a your turn...",a,flag,wrt);
-         if(flag) num++;
-         if(ismouseclick(WM_LBUTTONDOWN)){clearmouseclick(WM_LBUTTONDOWN); delay(20);}
-         continue;
          
      //ход бота;
      bot:
-     ncheck=0; flag=0; setcolor(14); outtextxy(420,280,"it's the bot's turn..."); 
-     setcolor(9); delay(800);
+     ncheck=0; flag=0; setcolor(14); outtextxy(420,280,"it's the bot's turn2..."); 
+     setcolor(9); num++;  //delay(800);
          
      //первый ход;
-     if(num==1){
+     /*if(num==2){
           if(a[5]==0){a[5]=4; circle(202,202,64); continue;}
           random=(rand()%4)+1;
           if(random==1){a[1]=4; circle(69,69,64); continue;}
@@ -410,7 +414,7 @@ void pvb(){
      }
          
      //второй ход;
-     if(num==2){ 
+     if(num==4){ 
        //проверка парных крестиков в строке;                   
        for(j=0;j<7;j+=3){     
         if((a[j+1]+a[j+2]+a[j+3])==2){
@@ -470,7 +474,7 @@ void pvb(){
    }
    
    //третий и четвёртый ходы;
-   if(num==3||num==4){
+   if(num==6||num==8){
                       
    //проверка парных ноликов в строке;                   
         for(j=0;j<7;j+=3){     
@@ -566,165 +570,21 @@ void pvb(){
      }
      
      //другие варианты;
-     casual_play(a,0);          
-   }     
+     casual_play(a,0);*/
+     int r=rand()%9+1;
+     zero(r); a[r]=4;          
+   //}    
   }
 }
- 
-//игрок против игрока;
-void pvp(){
-    int a[N],i,j,y=-68,prob=0,flag=0,sum,wrt=1;
-    for(i=1;i<=N;i++) a[i]=0;
-    setlinestyle(SOLID_LINE,2,THICK_WIDTH); 
-    score(1);
-     while(prob==0){
-         //проверка выигрыша крестика;
-          prob=win(a,3,"Player One WIN!",pscore,617,130);
-          if(prob==1) break;
-          //проверка выигрыша нолика;
-          prob=win(a,12,"Player One WIN!",bscore,617,180);
-          if(prob==1) break;
-         
-         //переход на ход второго игрока;
-         if(flag==1){            
-         if(ismouseclick(WM_LBUTTONDOWN)) clearmouseclick(WM_LBUTTONDOWN); delay(30);
-         goto PlayerTwo;
-         }
-         
-         //ход первого игрока; 
-         flag=play(3,"it's turn of the",a,flag,wrt);
-         if(ismouseclick(WM_LBUTTONDOWN)){clearmouseclick(WM_LBUTTONDOWN); delay(20);}
-         continue;
-         //ход второго игрока;
-         PlayerTwo:
-         
-         flag=play(4,"it's turn of the",a,flag,wrt);
-         if(ismouseclick(WM_LBUTTONDOWN)){clearmouseclick(WM_LBUTTONDOWN); delay(20);}
-         continue;          
-        }
-     }
-     
-//меню после игры;
-int finmenu(){
-     int flag=1;
-     setcolor(15);
-     settextstyle(4,0,1);
-     outtextxy(15,70,"YOU WANT TO CONTINUE");
-     outtextxy(150,100,"GAME?");
-     setcolor(2);
-     outtextxy(100,160,"YES!");
-     setcolor(4);
-     outtextxy(250,160,"NO!");
-     setcolor(14);
-     outtextxy(60,270,"EXIT TO MAIN MENU");
-     while(flag){                      
-      if(ismouseclick(WM_LBUTTONDOWN)&&mousex()>95&&mousex()<160&&mousey()>150&&mousey()<185){
-       return(1);
-      }
-      if(ismouseclick(WM_LBUTTONDOWN)&&mousex()>245&&mousex()<300&&mousey()>150&&mousey()<185){
-       return(0);
-      } 
-      if(ismouseclick(WM_LBUTTONDOWN)&&mousex()>55&&mousex()<370&&mousey()>265&&mousey()<300){                                                                                        
-       return(2);
-      } 
-     }
-    }
-     
+
 int main(){
-    srand(time(NULL));    
-//старновое меню;
-int smenu,pmenu,flag,check=1;
-menu:
-pscore=0; bscore=0; tscore=0; pmenu=0; smenu=0;      
-if(check==1) initwindow(405,405);           
-flag=1;
-check=0;
-clearmouseclick(WM_LBUTTONDOWN);     
-settextstyle(4,0,4);
-    outtextxy(100,120,"Play Game");
-    outtextxy(100,220,"Exit Game");
-    while(flag){
-    clearmouseclick(WM_LBUTTONDOWN);
-    delay(30);              
-    if(ismouseclick(WM_LBUTTONDOWN)&&mousex()>95&&mousex()<310&&mousey()>115&&mousey()<165){
-    flag=0;
-    smenu=1;                                            
+    int sum=0;
+    srand(time(NULL));
+initwindow(705,405);
+    while(1){
+      grid();
+      bvb();
+      cleardevice();
+      sum++;
+     }
     }
-    if(ismouseclick(WM_LBUTTONDOWN)&&mousex()>95&&mousex()<310&&mousey()>215&&mousey()<265){                                                
-    flag=0;
-    smenu=2;
-    }
-    }    
-if(smenu==1){
- clearmouseclick(WM_LBUTTONDOWN);
- cleardevice();
-    outtextxy(35,65,"Player vs Player");
-    outtextxy(65,145,"Player vs Bot");
-    outtextxy(65,225,"Bot vs Player");
-    outtextxy(150,305,"Back");
-    while(flag==0){
-    clearmouseclick(WM_LBUTTONDOWN);
-    delay(30);               
-    if(ismouseclick(WM_LBUTTONDOWN)&&mousex()>145&&mousex()<250&&mousey()>300&&mousey()<350){
-    flag=1;
-    cleardevice();                                                                                     
-    goto menu;                                                                                        
-    }
-    if(ismouseclick(WM_LBUTTONDOWN)&&mousex()>30&&mousex()<375&&mousey()>60&&mousey()<110){                                                                                       
-    flag=1;
-    pmenu=1; 
-    }
-    if(ismouseclick(WM_LBUTTONDOWN)&&mousex()>60&&mousex()<380&&mousey()>140&&mousey()<190){
-    flag=1;
-    pmenu=2;
-    }
-    if(ismouseclick(WM_LBUTTONDOWN)&&mousex()>60&&mousex()<380&&mousey()>220&&mousey()<270){
-    flag=1;
-    pmenu=3;     
-    }
-    }
-}
-//игрок против игрока;     
-if(pmenu==1){
-play:             
-closegraph();                                                          
-grid();    
-pvp();
-delay(1300);
-closegraph();
-initwindow(405,405);
-finmenu();
-if(finmenu()==1){closegraph(); goto play;}
-if(finmenu()==2){closegraph(); initwindow(405,405); goto menu;}
-}
-
-//игрок против бота; 
-if(pmenu==2){
-play_one:             
-closegraph();                                                           
-grid();    
-pvb();
-delay(1300);
-closegraph();
-initwindow(405,405);
-finmenu();
-if(finmenu()==1){closegraph(); goto play_two;}
-if(finmenu()==2){closegraph(); initwindow(405,405); goto menu;}
-}
-
-//Бот против игрока;
-if(pmenu==3){
-play_two:             
-closegraph();             
-grid();
-bvp();
-delay(1300);
-closegraph();
-initwindow(405,405);
-finmenu();
-if(finmenu()==1){closegraph(); goto play_one;}
-if(finmenu()==2){closegraph(); initwindow(405,405); goto menu;}
-}
-closegraph();     
-}
-
